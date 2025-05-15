0f7f7c75d33fc3a508d40cd63041515b7d8f30a5
Sofía <202215473@alu.comillas.edu>
2025-05-15 18:52:46 +0200

    probando una cosilla

--- myFirstApiRest/myFirstApiRest/settings.py
+++ myFirstApiRest/myFirstApiRest/settings.py
@@ -33,19 +33,18 @@ SECRET_KEY = 'django-insecure-2f7u)be-qwhok%0pc1)misghllj_bj-&=@y($zazur2krmk8tc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "https://empire-code-project.vercel.app",
 ]
-CORS_ALLOW_CREDENTIALS = True
-
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
     '0.0.0.0',
     # Lo de abajo es para desplegar en Render
     '@dpg-cvg8f5trie7s73bn9dhg-a.oregon-postgres.render.com',
-    'empire-code-backend-project.onrender.com'
+    'empire-code-backend.onrender.com'
 ]
 
 
